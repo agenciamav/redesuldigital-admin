3fd5a481cfc631368e73d6adbd9f1aae51c580a6
Luciano T <tonetlds@gmail.com>
2022-03-21 07:30:11 -0300

    Send answers to Google Sheets

--- routes/web.php
+++ routes/web.php
@@ -24,8 +24,13 @@ Route::get('/', function () {
     ]);
 });
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
+Route::get('/dashboard', function (\App\Services\GoogleSheets $googleSheets) {
+
+    $data = $googleSheets->getSheet('Worksheet');
+
+    return Inertia::render('Dashboard', [
+        'data' => $data,
+    ]);
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 require __DIR__.'/auth.php';
